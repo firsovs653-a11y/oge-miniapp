8eafd808f7e175d8df2e16da4fea8d17ac38a9e8
firsovs653-a11y <firsovs653@gmail.com>
2026-04-06 17:54:14 +0300

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///kinobase.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////app/instance/kinobase.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
