52b3f3a2d3f9e8ab0c2a99061dc003482802bf09
firsovs653-a11y <firsovs653@gmail.com>
2026-04-09 03:15:44 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,14 +2,13 @@
 
 echo "📦 Устанавливаю RepTube..."
 
-# Обновляем пакеты и устанавливаем зависимости
 apt-get update && apt-get install -y curl lua5.4 git make
 
-# Клонируем и устанавливаем RepTube
-git clone https://gitflic.ru/project/blogdron/reptube.git /tmp/reptube
+if [ ! -d "/tmp/reptube" ]; then
+    git clone https://gitflic.ru/project/blogdron/reptube.git /tmp/reptube
+fi
 cd /tmp/reptube && make install && cd /
 
 echo "✅ RepTube установлен"
 
-# Запускаем приложение
-python app.py
+cd /app && python app.py
